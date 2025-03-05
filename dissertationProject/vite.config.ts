d559c44e293a17815a112a9fd43bb0fdf868a842
Jiahui Xu <nemo5754@outlook.com>
2025-03-05 12:16:40 +0000

    change server port

--- dissertationProject/vite.config.ts
+++ dissertationProject/vite.config.ts
@@ -4,5 +4,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: '0.0.0.0',  // 监听所有网络接口
+    port: 3000,       // 端口号
+  },
 
 })
+
